273709f648c77cdbd5e4830c8d0718ac5e60cce8
Kurdi <sheriff.kurdi@gmail.com>
2022-03-28 10:20:39 +0200

    Categories Controller

---
+++ src/main/java/com/kurdi/inventoryservice/controllers/CategoriesController.java
@@ -0,0 +1,47 @@
+package com.kurdi.inventoryservice.controllers;
+
+import com.kurdi.inventoryservice.entities.Category;
+import com.kurdi.inventoryservice.repositories.CategoriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+@RestController
+@RequestMapping("/Categories/")
+public class CategoriesController {
+    @Autowired
+    CategoriesRepository categoriesRepository;
+
+    @GetMapping
+    public List<Category> getAll()
+    {
+        return  categoriesRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> addAuthority(@RequestBody Category category)
+    {
+
+        return new ResponseEntity<>(categoriesRepository.save(category), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{name}")
+    public ResponseEntity<Category> update(@PathVariable String name)
+    {
+        return new ResponseEntity<>(categoriesRepository.save(Category.builder().name(name).build()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{name}")
+    public ResponseEntity<Category> delete(@PathVariable String name)
+    {
+        Category category = Category.builder().name(name).build();
+        categoriesRepository.delete(category);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+
+}
