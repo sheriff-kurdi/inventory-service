94b8a76285c772af3a8dd94aa8d790d097692809
Kurdi <sheriff.kurdi@gmail.com>
2022-04-04 09:05:47 +0200

    security permissions

--- src/main/java/com/kurdi/inventoryservice/config/SecurityConfig.java
+++ src/main/java/com/kurdi/inventoryservice/config/SecurityConfig.java
@@ -2,49 +2,47 @@ package com.kurdi.inventoryservice.config;
 
 
 
-import com.kurdi.inventoryservice.auth.CustomAuthenticationProvider;
-import com.kurdi.inventoryservice.auth.filters.CustomUserNameAuthenticationFilter;
 import com.kurdi.inventoryservice.auth.filters.JwtTokenVerifierFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@SuppressWarnings("unused")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    CustomAuthenticationProvider authenticationProvider;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-/*
-        http.formLogin();
-*/
+
         http
                 .csrf().disable()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .addFilter(new CustomUserNameAuthenticationFilter(authenticationManager()))
-                .addFilterAfter(new JwtTokenVerifierFilter(), CustomUserNameAuthenticationFilter.class)
+                .addFilterBefore(new JwtTokenVerifierFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .mvcMatchers("/user")
                 .permitAll()
                 .mvcMatchers("/admin")
-                .hasAnyAuthority("admin");
+                .hasAnyAuthority("admin")
+                .mvcMatchers("/inventory/**")
+                .hasAnyAuthority("supplier")
+                .mvcMatchers(HttpMethod.GET,"/inventory/**")
+                .permitAll()
+                .mvcMatchers("/categories/**")
+                .hasAnyAuthority("admin")
+                .mvcMatchers(HttpMethod.GET,"/categories/**")
+                .permitAll();
 
 
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
-    }
+
 
 }
